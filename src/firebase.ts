cf75116f3534d2374b43589c0163d69a797dbe52
mampianina-andrianaivo <christianmampianina@gmail.com>
2026-07-14 16:04:55 +0300

    MAJ

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
-const metaEnv = (import.meta as any).env || {};
-
-const firebaseConfig = {
-  apiKey: metaEnv.VITE_FIREBASE_API_KEY || "",
-  authDomain: metaEnv.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: metaEnv.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: metaEnv.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: metaEnv.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: metaEnv.VITE_FIREBASE_APP_ID || "",
-  measurementId: metaEnv.VITE_FIREBASE_MEASUREMENT_ID || "",
-  firestoreDatabaseId: metaEnv.VITE_FIREBASE_FIRESTORE_DATABASE_ID || ""
-};
+import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = firebaseConfig.firestoreDatabaseId 
-  ? getFirestore(app, firebaseConfig.firestoreDatabaseId)
-  : getFirestore(app);
-
-
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);

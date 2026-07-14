c2803c5ae6202d6078879144e54f3d8f973bbd93
mampianina-andrianaivo <christianmampianina@gmail.com>
2026-07-14 15:57:18 +0300

    maj

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfigPlaceholder from '../firebase-applet-config.json';
 
 const metaEnv = (import.meta as any).env || {};
 
 const firebaseConfig = {
-  apiKey: metaEnv.VITE_FIREBASE_API_KEY || firebaseConfigPlaceholder.apiKey || "",
-  authDomain: metaEnv.VITE_FIREBASE_AUTH_DOMAIN || firebaseConfigPlaceholder.authDomain || "",
-  projectId: metaEnv.VITE_FIREBASE_PROJECT_ID || firebaseConfigPlaceholder.projectId || "",
-  storageBucket: metaEnv.VITE_FIREBASE_STORAGE_BUCKET || firebaseConfigPlaceholder.storageBucket || "",
-  messagingSenderId: metaEnv.VITE_FIREBASE_MESSAGING_SENDER_ID || firebaseConfigPlaceholder.messagingSenderId || "",
-  appId: metaEnv.VITE_FIREBASE_APP_ID || firebaseConfigPlaceholder.appId || "",
-  measurementId: metaEnv.VITE_FIREBASE_MEASUREMENT_ID || firebaseConfigPlaceholder.measurementId || "",
-  firestoreDatabaseId: metaEnv.VITE_FIREBASE_FIRESTORE_DATABASE_ID || (firebaseConfigPlaceholder as any).firestoreDatabaseId || ""
+  apiKey: metaEnv.VITE_FIREBASE_API_KEY || "",
+  authDomain: metaEnv.VITE_FIREBASE_AUTH_DOMAIN || "",
+  projectId: metaEnv.VITE_FIREBASE_PROJECT_ID || "",
+  storageBucket: metaEnv.VITE_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: metaEnv.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: metaEnv.VITE_FIREBASE_APP_ID || "",
+  measurementId: metaEnv.VITE_FIREBASE_MEASUREMENT_ID || "",
+  firestoreDatabaseId: metaEnv.VITE_FIREBASE_FIRESTORE_DATABASE_ID || ""
 };
 
 const app = initializeApp(firebaseConfig);
